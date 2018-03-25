92a32ce0584a0848f75d5fd617990579b3bb7341
Jimmy Van Veen <jimmy.van.veen@gmail.com>
2018-03-24 21:59:33 -0400

    Port from Codepen complete

--- src/carousel/carousel-item.js
+++ src/carousel/carousel-item.js
@@ -5,7 +5,7 @@ export default class CarouselItem extends Component {
   render() {
     return (
       <li className='carousel-item'>
-        <h1>Project {this.props.id}</h1>
+        <h1>Project {this.props.id + 1}</h1>
         <h2>Does Great Things</h2>
         <p>This is a description for the project</p>
       </li>
